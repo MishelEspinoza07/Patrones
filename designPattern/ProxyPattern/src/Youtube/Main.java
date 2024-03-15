017fd07fc021c21880c5c53f8033660d0d2e1d50
MishelEspinoza07 <102001751+MishelEspinoza07@users.noreply.github.com>
2024-03-15 14:29:45 -0400

    Patron Proxy

---
+++ designPattern/ProxyPattern/src/Youtube/Main.java
@@ -0,0 +1,30 @@
+package Youtube;
+
+public class Main {
+
+	public static void main(String[] args) {
+        YouTubeDownloader naiveDownloader = new YouTubeDownloader(new ThirdPartyYouTubeClass());
+        YouTubeDownloader smartDownloader = new YouTubeDownloader(new YouTubeCacheProxy());
+
+        long naive = test(naiveDownloader);
+        long smart = test(smartDownloader);
+        System.out.print("Time saved by caching proxy: " + (naive - smart) + "ms");
+
+    }
+
+    private static long test(YouTubeDownloader downloader) {
+        long startTime = System.currentTimeMillis();
+
+        downloader.renderPopularVideos();
+        downloader.renderVideoPage("dogZzzz");
+        downloader.renderPopularVideos();
+        downloader.renderVideoPage("Turing Machine video");
+        downloader.renderVideoPage("dogZzzz");
+        downloader.renderVideoPage("nothing");
+
+        long estimatedTime = System.currentTimeMillis() - startTime;
+        System.out.print("Time elapsed: " + estimatedTime + "ms\n");
+        return estimatedTime;
+    }
+}
+
